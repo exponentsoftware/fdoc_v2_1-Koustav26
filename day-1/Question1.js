47d4d9345e770ffd463cb8a542fbfec816a8a137
Koustav <koustavbasak94@gmail.com>
2023-05-14 02:37:24 +0530

    Day 1 Question 1

---
+++ day-1/Question1.js
@@ -0,0 +1,32 @@
+/*
+1.a. Write a function called `countOccurrences` that takes an array of numbers and a number as input and returns the number of times the input number occurs in the array.
+```js
+const arr = [4, 2, 6, 8, 4, 1, 4];
+console.log(countOccurrences(arr, 4));
+3
+*/
+console.log("hello");
+/*
+
+const arr = [4, 2, 6, 8, 4, 1, 4];
+
+const countOccurrences = (arr, number) => {
+  let count = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === number) {
+      count++;
+    }
+  }
+  return count;
+};
+
+console.log(countOccurrences(arr, 4));
+*/
+
+// const arr = [4, 2, 6, 8, 4, 1, 4];
+
+// const countOccurrences = (arr,number) =>{
+//     let occursArray = [];
+
+//     let result
+// }
